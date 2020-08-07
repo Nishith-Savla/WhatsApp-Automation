98e21137faaacb8ba97a49d77ae02360177a1fa8
Nishith-Savla <nishithsavla005@gmail.com>
2020-08-07 15:56:25 +0530

    Resolved bug that allowed only to send messages to chats in the front
    Now it will first search the person so that he/she comes into the screen and then it will send the message

--- sendMessage.py
+++ sendMessage.py
@@ -1,7 +1,9 @@
-import time
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
-from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
 
 def check_login():
     is_connected = input("Have you signed in WhatsApp web? (Yes/No): ")
@@ -10,13 +12,13 @@ def check_login():
             driver.find_element_by_xpath('//*[@id="side"]/header/div[1]/div/img')
             print("Login successful")
             return True
-        except:
+        except NoSuchElementException:
             print("It doesn't seem you have logged in")
             print('Try logging in again')
-            time.sleep(1.5)
-            check_login()
+            return False
+        except:
+            return False
     else:
-        time.sleep(1.5)
         check_login()
 
 
@@ -24,18 +26,21 @@ option = webdriver.ChromeOptions()
 driver = webdriver.Chrome("C:\\Users\\nishi\\chromedriver.exe")
 driver.get("https://web.whatsapp.com/")
 
-if check_login():
-    user = input("Whom do you want to send the message to: ")
-    found = False
-    while not found:
-        try:
-            driver.find_element_by_xpath('//span[@title = "{}"]'.format(user)).click()
-            found = True
-            msg = input("Enter the message to send: ")
-            msg_box = driver.find_element_by_xpath('//*[@id="main"]/footer/div[1]/div[2]/div/div[2]')
-            count = int(input("How many times do you want to send the message: "))
-            for i in range(count):
-                msg_box.send_keys(msg)
-                driver.find_element_by_xpath('//*[@id="main"]/footer/div[1]/div[3]/button').click()
-        except NoSuchElementException:
-            print(user + " not found")
+while not check_login():
+    driver.implicitly_wait(2)
+
+user = input("Whom do you want to send the message to: ")
+try:
+    driver.find_element_by_xpath('//*[@id="side"]/div[1]/div/label/div/div[2]').send_keys(user)
+    x_path = '//span[@title = "{}"]'.format(user)
+    WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH, x_path))).click()
+    msg = input("Enter the message to send: ")
+    msg_box = driver.find_element_by_xpath('//*[@id="main"]/footer/div[1]/div[2]/div/div[2]')
+    count = int(input("How many times do you want to send the message: "))
+    for i in range(count):
+        msg_box.send_keys(msg)
+        driver.find_element_by_xpath('//*[@id="main"]/footer/div[1]/div[3]/button').click()
+except NoSuchElementException:
+    print(user + " not found")
+finally:
+    driver.quit()
