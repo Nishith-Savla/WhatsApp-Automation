c59eace555ea5abe34cb3422dd7d07f27e790e09
Nishith-Savla <nishithsavla005@gmail.com>
2020-08-08 16:33:25 +0530

    Add feature to select message content from a file and logout after sending if user wishes
    Also add extensions to the browser from the computer

--- sendMessage.py
+++ sendMessage.py
@@ -1,71 +1,94 @@
-import time
+from time import sleep  # for stopping the program for an interval
 
+# for controlling the browser
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support import expected_conditions as ec
 from selenium.webdriver.support.wait import WebDriverWait
 
 
-def check_login() -> bool:
+def check_login(p_driver) -> bool:
+    """Returns True if the user has successfully logged in to WhatsApp, else False
+    :return True or False
+
+    """
     is_connected = input("Have you signed in WhatsApp web? (Yes/No): ")
     if is_connected.lower().startswith('yes'):
         # noinspection PyBroadException
         try:
-            driver.find_element_by_xpath('//*[@id="side"]/header/div[1]/div/img')
+            p_driver.find_element_by_xpath('//*[@id="side"]/header/div[1]/div/img')
             print("Login successful")
             return True
         except NoSuchElementException:
             print("It doesn't seem you have logged in")
             print('Try logging in again')
             return False
-        except Exception:
-            print(Exception)
-            return False
     else:
         return False
 
 
-def send_to_everyone(name, message, count):
+def send(p_driver, name, message, count=1):
+    """Sends the message to the user for 'count' number of times"""
     try:
-        driver.find_element_by_xpath('//*[@id="side"]/div[1]/div/label/div/div[2]').send_keys(name)
-        WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.XPATH, '//span[@title = "{}"]'.format(name)))).click()
-        msg_box = driver.find_element_by_xpath('//*[@id="main"]/footer/div[1]/div[2]/div/div[2]')
+        p_driver.find_element_by_xpath('//*[@id="side"]/div[1]/div/label/div/div[2]').send_keys(name)
+        WebDriverWait(p_driver, 20).until(
+            ec.presence_of_element_located((By.XPATH, '//span[@title = "{}"]'.format(name)))).click()
+        msg_box = p_driver.find_element_by_xpath('//*[@id="main"]/footer/div[1]/div[2]/div/div[2]')
         for i in range(count):
             msg_box.send_keys(message)
-            driver.find_element_by_xpath('//*[@id="main"]/footer/div[1]/div[3]/button').click()
-    except NoSuchElementException:
+            p_driver.find_element_by_xpath('//*[@id="main"]/footer/div[1]/div[3]/button').click()
+    except Exception:
         print(name + " not found")
     finally:
-        driver.implicitly_wait(5)
+        p_driver.implicitly_wait(5)
 
 
+def run(p_driver):
+    """Runs the whole program"""
+    p_driver.get("https://web.whatsapp.com/")
+    while not check_login(p_driver):
+        sleep(2)
+
+    choose_from_file = input("Do you want to choose the list of people from a file: ")
+    user_list = []
+    if choose_from_file.lower().startswith("yes"):
+        filepath = input("Enter the file path: ")
+        with open(filepath, "r") as file:
+            for line in file:
+                user_list.append(line)
+    elif choose_from_file.lower().startswith("no"):
+        names = input(
+            "Enter the names of people you want to send the message to (separated by ', ' if more than one): ")
+        user_list = names.split(', ')
+
+    msg = ""
+    choose_from_file = input("Do you want to choose the message from a file: ")
+    if choose_from_file.lower().startswith("yes"):
+        filepath = input("Enter the file path: ")
+        with open(filepath, "r") as file:
+            for line in file:
+                msg += line
+    elif choose_from_file.lower().startswith("no"):
+        msg = input("Enter the message to send: ")
+
+    send_count = int(input("How many times do you want to send the message: "))
+
+    for user in user_list:
+        send(p_driver, user.lstrip().rstrip(), msg, send_count)
+
+    wanna_quit = input("Do you want to logout (NOTE: This may stop sending any unsent "
+                       "messages): ")
+    if wanna_quit.lower().startswith("yes"):
+        p_driver.find_element_by_xpath('//*[@id="side"]/header/div[2]/div/span/div[3]/div').click()
+        p_driver.find_element_by_xpath('//*[@id="side"]/header/div[2]/div/span/div[3]/span/div/ul/li[7]/div').click()
+        p_driver.quit()
+
+
+# Chrome options
 option = webdriver.ChromeOptions()
-driver = webdriver.Chrome("C:\\Users\\nishi\\chromedriver.exe")
-driver.get("https://web.whatsapp.com/")
-
-while not check_login():
-    time.sleep(2)
-
-choose_from_file = input("Do you want to choose the list of people from a file: ")
-user_list = []
-if choose_from_file.lower().startswith("yes"):
-    filepath = input("Enter the file path: ")
-    file = open(filepath, "r")
-    for line in file:
-        line = line.rstrip()
-        user_list.append(line)
-elif choose_from_file.lower().startswith("no"):
-    names = input("Enter the names of people you want to send the message to (separated by ', ' if more than one): ")
-    user_list = names.split(', ')
-
-msg = input("Enter the message to send: ")
-send_count = int(input("How many times do you want to send the message: "))
-
-for user in user_list:
-    if user.endswith(' '):
-        user.rstrip(' ')
-    send_to_everyone(user, msg, send_count)
-
-driver.quit()
+# You can add extensions to the browser by passing its path here
+option.add_extension("EXTENSION PATH")
+driver = webdriver.Chrome("CHROMEDRIVER PATH", options=option)
+if __name__ == "__main__":
+    run(driver)
